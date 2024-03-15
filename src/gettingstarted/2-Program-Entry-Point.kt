b81e955c754ead60b8c51dc378fe200754ee6448
Gamze Coşkun <hi@codersclub.com>
2024-03-16 02:48:10 +0300

    gettingstarted package added

---
+++ src/gettingstarted/2-Program-Entry-Point.kt
@@ -0,0 +1,20 @@
+package gettingstarted
+
+/**
+ * JVM projelerinin giris noktasi main fonksiyonudur.
+ */
+
+fun main(){
+    println("Kekod-The education you've always dreamed of")
+}
+
+//-----------------------------------------------------------------------------------------------------------------
+
+/**
+ *  @param args ile gerekiyorsa baslangic anina sabit parametrelerinizi gonderebilirsiniz.
+ *  Edit configurations.. Configuration :Tab -> Program Arguments: -> icerisine parametreleri girebilirsiniz
+ */
+
+//fun main(args:Array<String>){
+//
+//}
